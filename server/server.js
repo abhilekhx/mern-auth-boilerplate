f105f43a38bd26055360711147ca46172e37e22c
Abhilekh Adhikari <abhilekhadhikari@MacBook-Pro.local>
2024-06-03 23:56:50 -0400

    applying all middlewares - morgan,cors,mongoose,bodyparser. Set up .env file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+
+require("dotenv").config();
 
 const app = express();
 
 // import routes
 const authRoutes = require("./routes/auth");
+const bodyParser = require("body-parser");
+
+// app middleware
+app.use(morgan("dev")); // adds logging functionality
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 
 // middleware
 app.use("/api", authRoutes);
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
+const nodeEnv = process.env.NODE_ENV;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port} - ${nodeEnv}`);
 });
